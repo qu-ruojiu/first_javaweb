99fdab9e5e6996a6c97939bd155ebddf805399fa
qu-ruojiu <1367625765@qq.com>
2020-07-14 11:31:02 +0800

    赋初值，建立起始点，蛇出生点以及食物出身点

--- web/area.js
+++ web/area.js
@@ -38,3 +38,33 @@ var snakeLength;  //蛇身长度
 var ifGrowing;  //上一次是否长了身体
 var setIntervalReturn;  //记录setInterval的返回值
 
+function init()  //按开始键初始化数据
+{
+    scores = 0;
+    document.getElementById("scores").value = 0;
+    grade = 1;
+    document.getElementById("grade").value = 1;
+    speed = 250;
+    topSpeedMode = false;
+    snakeLength = 1;
+    ifGrowing = false;
+    dir = [1, 0];  //一开始向右
+    oldKeyCode = 39;  //一开始向右
+    pressWating = true;  //用于防止方向键被快速按多次
+    clearInterval(setIntervalReturn);  //防止多次重按start造成多个setInterval同时进行
+}
+
+function originalBodyAndFood()  //先造出3个长度和食物
+{
+    snake = new Array();
+
+    snake[0] = new Snake(3, Math.floor(y/2)+2);
+    snake[0].getHTML().style.backgroundColor = headColor;
+    gameArea[snake[0].y][snake[0].x].isHead = true;
+
+    gameArea[Math.floor(y/2-1)][Math.floor(x/2)].isFood = true;
+    gameArea[Math.floor(y/2-1)][Math.floor(x/2)].getHTML().style.backgroundColor = foodColor;
+
+    for (var i=1; i<3; i++)
+        setTimeout("growing()", speed*i);
+}
